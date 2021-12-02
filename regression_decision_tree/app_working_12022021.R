1f0c6b1f54d1f0358678052935d7f4eda901321c
JackJosephWright <jwrightzz1234@gmail.com>
2021-12-02 15:46:32 -0500

    more work on better file

--- regression_decision_tree/app_working_12022021.R
+++ regression_decision_tree/app_working_12022021.R
@@ -15,7 +15,7 @@ library(stringr)
 
 ui = fluidPage(
   
-  tabsetPanel(
+  tabsetPanel( id='tabSwitch',
     ## data input tab
           tabPanel("Managing Response and Predictor Variables", fluid = TRUE,
                    sidebarLayout(
@@ -108,35 +108,72 @@ ui = fluidPage(
                      ) #close mainPanel
                    ) # close sidebarLayout
                    
-                   ) # close transformation tab
+                   ),# close transformation tab
+          tabPanel("Modeling", fluid = TRUE,
+                   sidebarLayout(
+                     sidebarPanel(),
+                     mainPanel(
+                       h1('base model'),
+                       fluidRow(
+                         splitLayout(style = "border: 1px solid silver;",cellWidths = c("20%", "40%","40%"),
+                                     verbatimTextOutput('base.mod'),
+                                     plotOutput('base.resid')
+                         )
+                       )
+                   )
+                   )
+          )#close modeling tab
+          
       
-          ) # close tabsetPanel
+          ), # close tabsetPanel
  
 )#fluidPage Close
 
 
 server<-function(input, output, session){
   ## FUNCTIONS
-  t.specific<-function(){
+  t.specific<-function(action=NULL,i=0){
     action_button_list<-list('x1','x2','r')
     #transform column
     #get column
-    col<-as.data.frame(df.t()[toAccept()])
+    if(action=='accept'){
+    col<-as.data.frame(df.t.static()[toAccept()])
     
     #identify which button to check 
     transform_func<-paste0('t_type_',action_button_list[toAccept()])
+    
     fn<-input[[transform_func]]
-    #check is not 'none', if 'none'break
     if(fn=='none'){
+      
       toAccept(NULL)
       return(NULL)
       
     }
+    #check is not 'none', if 'none'break
+    }
+    if(action=='plot'){
+      col<-as.data.frame(df.t.static()[i])
+      transform_func<-paste0('t_type_',action_button_list[i])
+      fn<-input[[transform_func]]
+      
+      
+    }
+    
+    
+    
     
     #put column and fn into transform_column()
+    
     t.col<-transform_column(col,fn)
-    return(t.col)
+    if(action=='accept'){
+    return(list(t.col,fn))
+    }else{
+      
+        return(t.col)
+      }
   }
+  
+  
   transform_column<-function(column,fn){
     #transforms a single column and returns a named vector
     if(fn=='log'){
@@ -145,8 +182,12 @@ server<-function(input, output, session){
     if(fn=='sqrt'){
       return(sqrt_transform(column))
     }
-    else{
+    if(fn=='poly'){
       return(poly_transform(column))
+    }
+    else{
+      message('made it to `else` statement in transform_column')
+      return(column)
     }
     
     
@@ -156,7 +197,7 @@ server<-function(input, output, session){
     if(minval>0){
     return(log(column))
     }else{
-      column<-column(abs(min_val)+.01)
+      column<-column(abs(minval)+.01)
       return(log(column))
     }
   }
@@ -165,7 +206,7 @@ server<-function(input, output, session){
     if(minval>0){
       return(sqrt(column))
     }else{
-      column<-column+(abs(min_val)+.01)
+      column<-column+(abs(minval)+.01)
       return(sqrt(column))
     }
   }
@@ -212,27 +253,38 @@ server<-function(input, output, session){
   p_selected <-reactiveVal()
   ## transformation tab
   
-  ## transformation tab
+  ## transformation tab reactive elements
   
   df.t<-reactiveVal()
+  df.t.static<-reactiveVal()
   toListen<-reactive({
     #listener for change of input on plots
+    #message('toListen() triggered')
     list(input$x1,input$x2)
   })
   plot.t.Listen<-reactive({
     list(input$t_type_x1,input$t_type_x2,input$t_type_r)
   })
   
+  
   toAccept<-reactiveVal({
     #listener that a transformation has been accepted
     
   })
-  df.t.accepted<-reactiveVal()
+  df.t.accepted<-reactiveVal({
+    NULL
+  })
   
   #df.t.plot for plotting transformations
   #df.t.plot()<-reactiveVal()
   
+  ## model tab reactive elements
   
+  mod.list<-reactiveVal({
+    m.list<-list()
+  df.base.mod<-reactiveVal({})
+  df.trans.mod<-reactiveVal()
+  })
   ## OBSERVERS
   
     ##### data input tab#####
@@ -274,6 +326,8 @@ server<-function(input, output, session){
   
   ####transformations tab####
   
+  t.response.name<-reactiveVal()
+  
   observeEvent(input$p_vars,{
     # set values on transformation page
     updateSelectInput(session, 
@@ -297,6 +351,7 @@ server<-function(input, output, session){
                        't_type_x2',
                        selected='none')
     df.t.accepted(NULL)
+    t.response.name(NULL)
     #delete transformation table
     
     
@@ -304,27 +359,41 @@ server<-function(input, output, session){
   
   observeEvent(toListen(),{
     #create plot frame 
+    #message('df.t.static triggered')
     d<-data.frame(cbind(raw_data()[input$x1],raw_data()[input$x2],raw_data()[input$response_var]))
-    df.t(d)
+    #message(paste('df.t.static() columns:',colnames(d)))
+    df.t.static(d)
     
     #message('colnames df.t:',colnames(df.t()))
     
   })
+  observeEvent(df.t.static(),{
+    
+    updateRadioButtons(session,
+                       't_type_x1',
+                       selected='none')
+    updateRadioButtons(session,
+                       't_type_x2',
+                       selected='none')
+    df.t(df.t.static())
+  })
   observeEvent(plot.t.Listen(),{
-    #transform plot variables
-    transformation_list<-list(input$t_type_x1,input$t_type_x1,input$t_type_r)
-    for(i in 1:3){
-      if(transformation_list[i]!='none'){
-        message(names(df.t()[i]))
+    df.local<-df.t()
+    if(!is.null(predictor_list())){
+      for(i in 1:3){
+        col<-t.specific(action='plot',i=i)
+        df.local[i]<-col
+        
       }
+    df.t(df.local)
     }
   })
   
-  observeEvent(toListen(),{
+  observeEvent(df.t(),{
     #generate x1 vs response
     output$r_x1<-renderPlot({showplot1(df.t(),1,3)})
   })
-  observeEvent(toListen(),{
+  observeEvent(df.t(),{
     output$x1_x2<-renderPlot(showplot1(df.t(),1,2))
   })
   
@@ -350,35 +419,28 @@ server<-function(input, output, session){
     
   })
   
+  
  
   # generate df.t.accep
   observeEvent(toAccept(),{
-    t.col1<-t.specific()
-    if(is.null(t.col1)){
+    t.col_list<-t.specific(action='accept')
+    
+    if(is.null(t.col_list)){
       return(NULL)
     }
-    action_button_list<-list('x1','x2','r')
-    #transform column
-    #get column
-    col<-as.data.frame(df.t()[toAccept()])
+    t.col<-as.data.frame(t.col_list[1])
     
-    #identify which button to check 
-    transform_func<-paste0('t_type_',action_button_list[toAccept()])
-    fn<-input[[transform_func]]
-    #check is not 'none', if 'none'break
-    if(fn=='none'){
-      toAccept(NULL)
-      stop('execution stopped because fn==none')
-      
-    }
+    fn<-t.col_list[2]
     
-    #put column and fn into transform_column()
-    t.col<-transform_column(col,fn)
-    #rename the column something descriptive with the transformation
+    
+    
     oldname<-names(t.col)
+    
     newname<-paste0(fn,'(',names(t.col),')')
     colnames(t.col)<-newname
-    
+    if(oldname==input$response_var){
+      t.response.name(newname)
+    }
     #check that the column doesnt already exist in df.t.accepted
     if(newname%in%colnames(df.t.accepted())){
       toAccept(NULL)
@@ -407,8 +469,53 @@ server<-function(input, output, session){
   })
   
   
-  observe({
-    
+  ## models observers
+  
+  
+  
+  
+ 
+  
+  observeEvent(input$tabSwitch, {
+    if(!is.null(predictor_list())){
+    if(input$tabSwitch=='Modeling'){
+      
+      #generate df.base.mod
+      df.base.mod(raw_data()%>%select(p_selected(),input$response_var))
+      #generate df.trans.mod
+      
+      if(length(df.t.accepted()>0)){
+        if(grepl(input$response_var,colnames(df.t.accepted()))){
+        df.trans.mod(cbind(df.base.mod()%>%select(-input$response_var),df.t.accepted())%>%relocate(t.response.name(),.after = last_col()))
+        }else{
+            df.trans.mod(cbind(df.base.mod(),df.t.accepted()))
+          }
+      }else{
+        df.trans.mod(df.base.mod())
+          
+        }
+      
+      message(colnames(df.trans.mod()))
+    }
+      
+      }
+  })
+  
+  observeEvent(df.trans.mod(),{
+    message('made it into the model list observer')
+    formula_base<-paste0(input$response_var,'~.')
+    #message(formula_base)
+    m<-list()
+    m[['base']]<-lm(formula_base,data=df.base.mod())
+    if(is.null(t.response.name())){
+      t.response<-input$response_var
+    }else{
+      t.response<-t.response.name()
+    }
+    formula_trans<-paste0(t.response,'~.')
+    m[['trans']]<-lm(formula_trans, data=df.trans.mod())
+    mod.list(m)
+    message(names(mod.list()))
   })
   
   
